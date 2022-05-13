e0aa8b554289009d60e3c08920f053b1df7b6e49
FastAsFuck <83665402+fastasfuck@users.noreply.github.com>
2022-05-13 14:17:31 +0300

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-apt-get install nginx -y
+sudo apt-get install nginx -y
+sudo apt-get install python3 -y
+sudo apt-get install pip3 -y
+sudo apt-get install git
+mkdir TEST
+cd TEST
+git clone https://github.com/fastasfuck/labs6.git
+cd labs6/
+sudo cp index.html /var/www/html/index.nginx-debian.html
+sudo systemctl restart nginx.service
